6d3425ee51796a60b024454c5526eeb6ad909c80
sara-dme <s.daoumy@gamil.com>
2024-04-19 19:50:57 +0100

    fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,6 +23,7 @@ if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth import BasicAuth
     auth = BasicAuth()
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """ Module of Basic Authentication
 """
 from api.v1.auth.auth import Auth
-import base64 
+import base64
 from models.user import User
 from typing import TypeVar
 
