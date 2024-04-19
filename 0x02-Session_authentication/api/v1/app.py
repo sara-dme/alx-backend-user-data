23e783c52e5e3076f5ff70823d80c5fc09a3ee0b
sara-dme <s.daoumy@gamil.com>
2024-04-19 22:16:24 +0100

    takss

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,7 @@ if getenv('AUTH_TYPE') == 'basic_auth':
     auth = BasicAuth()
 
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
@@ -52,12 +53,14 @@ def before_request() -> str:
     """
     if auth is None:
         return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
         abort(401)
+    request.current_user = auth.current_user(request)
     if auth.current_user(request) is None:
         abort(403)
 
