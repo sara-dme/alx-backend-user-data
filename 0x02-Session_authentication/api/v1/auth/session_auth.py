85a905d161c1134f8849196cee5f5c764f29425c
sara-dme <s.daoumy@gamil.com>
2024-04-20 18:48:34 +0100

    fix

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module for authentication using Session auth
+"""
+
+
+from .auth import Auth
+
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """ documentation doc doc """
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ documentation__"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
+        
