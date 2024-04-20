3a70cbc532abd8b6e0b0b5e7198997bba04f6314
sara-dme <s.daoumy@gamil.com>
2024-04-20 19:32:22 +0100

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,7 +15,6 @@ class SessionAuth(Auth):
 
     user_id_by_session_id = {}
 
-
     def create_session(self, user_id: str = None) -> str:
         """ documentation__"""
         if user_id is None or not isinstance(user_id, str):
@@ -24,12 +23,31 @@ class SessionAuth(Auth):
         id = uuid4()
         self.user_id_by_session_id[str(id)] = user_id
         return str(id)
-    
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
-         """ documentation ___"""
-         if session_id is None or not isinstance(session_id, str):
+        """ documentation ___"""
+        if session_id is None or not isinstance(session_id, str):
             return None
-         return self.user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
+    
+    def current_user(self, request=None):
+        """__doc__"""
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        user = User.get(user_id)
+        return user
+    
 
-
+    def destroy_session(self, request=None):
+        """_summary_ doc
+        """
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
