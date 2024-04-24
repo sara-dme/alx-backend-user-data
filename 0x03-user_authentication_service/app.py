f2c7449c4ee9691e80e1a76de470abf1f73d40eb
sara-dme <s.daoumy@gamil.com>
2024-04-24 18:24:20 +0100

    task 10

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 """Module basic flask app"""
 
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, make_response, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
 AUTH = Auth()
 
+@app.route("/sessions", methods=['POST'])
+def login() -> str:
+    """ log in and return session id"""
+    try:
+        email = request.form.get['email']
+        password  = request.form.get['password']
+    except KeyError:
+        abort(400)
+    if AUTH.valid_login(email, password):
+        response = make_response(jsonify({"email": email, "message": "logged in"}), 200)
+        response.set_cookie("session_id", AUTH.create_session(email))
+        return response
+    abort(401)
 
 @app.route('/', methods=["GET"])
 def hello_world():
