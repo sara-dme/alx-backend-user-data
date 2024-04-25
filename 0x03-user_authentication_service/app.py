15a46702abe7b5fb6148ab4bfdc3883f58a162fc
sara-dme <s.daoumy@gamil.com>
2024-04-25 10:54:12 +0100

    test the code

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -55,5 +55,17 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """200 HTTP status if the user exist
+    otherwise respond with 403 HTTP status"""
+    session_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
