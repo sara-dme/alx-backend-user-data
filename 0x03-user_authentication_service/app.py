a8298f9e1368d3b55e91ebcd9e8ae2f96f8d5a70
sara-dme <s.daoumy@gamil.com>
2024-04-25 12:19:40 +0100

    pycodestyle

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -92,7 +92,7 @@ def update_password() -> str:
     email = request.form['email']
     reset_token = request.form['reset_token']
     new_password = request.form['new_password']
-   
+
     try:
         AUTH.update_password(reset_token, new_password)
     except ValueError:
